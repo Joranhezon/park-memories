85b4d0006776f6cd895139fed07d2505181b5959
Marina Joranhezon <marina.joranhezon@gmail.com>
2022-04-05 15:50:03 -0400

    Add google maps search on home page

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["map"]
+    static targets = ["field", "map", "latitude", "longitude"]
 
   connect() {
     if (typeof (google) != "undefined") {
@@ -10,28 +10,63 @@ export default class extends Controller {
   }
 
   initializeMap() {
-    this.initMap()
-    this.putMarker()
+    this.map()
+    this.setMarker()
+    this.autoComplete()
   }
 
-  initMap() {
-    this.map = new google.maps.Map(this.mapTarget, {
-      center: new google.maps.LatLng(-15.7961, -47.9252),
-      zoom: 17
-    })
+  map() {
+    if(this._map == undefined) {
+      this._map = new google.maps.Map(this.mapTarget, {
+        center: new google.maps.LatLng(-15.7961, -47.9252),
+        zoom: 17
+      })
+    }
 
-    return this.map
+    return this._map
   }
 
-  putMarker() {
-    this.marker = new google.maps.Marker({
-      map: this.map,
-      anchorPoint: new google.maps.Point(0,0)
-    })
+  setMarker() {
+    if(this._marker == undefined) {
+      this._marker = new google.maps.Marker({
+        map: this.map(),
+        anchorPoint: new google.maps.Point(0,0)
+      })
 
-    this.marker.setPosition({lat: -15.7961, lng: -47.9252})
-    this.marker.setVisible(true)
+      this._marker.setPosition({lat: -15.7961, lng: -47.9252})
+      this._marker.setVisible(true)
+    }
 
-    return this.marker
+    return this._marker
+  }
+
+  autoComplete() {
+    if(this._autocomplete == undefined) {
+      this._autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
+      this._autocomplete.bindTo('bounds', this.map())
+      this._autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
+      this._autocomplete.addListener('place_changed', this.locationChanged.bind(this))
+    }
+
+    return this._autocomplete
+  }
+
+  locationChanged() {
+    let place = this.autoComplete().getPlace()
+
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+
+    this.map().fitBounds(place.geometry.viewport)
+    this.map().setCenter(place.geometry.location)
+    this.setMarker().setPosition(place.geometry.location)
+    this.setMarker().setVisible(true)
+
+    this.latitudeTarget.value = place.geometry.location.lat()
+    this.longitudeTarget.value = place.geometry.location.lng()
   }
 }
